55362c0d1d7789888a42b4d7e180efeac3ab7745
putriariyanti156-tech <putriariyanti156@gmail.com>
2026-09-04 15:51:48 +0700

    Add files via upload

---
+++ putri ariyanti/tugas 1.py
@@ -0,0 +1,15 @@
+# Program Menghitung Luas dan Keliling Taman Persegi Panjang
+
+# Input
+panjang = 20
+lebar = 12
+
+# Proses
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+# Output
+print("Panjang taman =", panjang, "meter")
+print("Lebar taman =", lebar, "meter")
+print("Luas taman =", luas, "m²")
+print("Keliling taman =", keliling, "meter")
